0befe6043be31e6a782f6bb4938eb9714881a23b
Ryan Loader <ryan.loader@oneleggedcrab.com>
2017-01-21 14:09:51 +1300

    Continually wrap the water

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 color = require("color")
 
+DEBUG_DRAW_WATER_POLY = true
+DEBUG_DRAW_NO_BG = true
+
 KEY_RESTART = "r"
 KEY_QUIT = "escape"
 
@@ -7,21 +10,22 @@ COL_WHITE = color.rgb(255,255,255)
 COL_WATER = color.rgb(41,63,101)
 
 REST_Y = love.graphics.getHeight()*0.7
+NUMBER_OF_WAVE_POINTS = 100
+DIST_BETWEEN_WATER_POINTS = love.graphics.getWidth() / NUMBER_OF_WAVE_POINTS * 2
 
 function love.load()
 	if assets == nil then
 		assets = require("assets")
 	end
 
+	wave_offset = 0
 	wave_points = {}
-	number_of_wave_points = 100
-
-	local xinc = love.graphics.getWidth() / number_of_wave_points * 2
-	for i=1,number_of_wave_points do
-		local xx = (i-1)*xinc
+	for i=1,NUMBER_OF_WAVE_POINTS do
+		local xx = (i-1) * DIST_BETWEEN_WATER_POINTS
 		local yy = REST_Y
 
 		wave_points[i] = {
+			i = i,
 			x = xx,
 			y = yy,
 			dist = math.sin(xx * 0.01) * 100
@@ -32,27 +36,41 @@ end
 
 function love.update(dt)
 	local sp = dt * 90
-
+	wave_offset = wave_offset - sp
 	for i,pt in ipairs(wave_points) do
-		pt.x = pt.x - sp
-		pt.y = REST_Y + pt.dist + math.sin(pt.x * 0.02) * 10
+		pt.y = REST_Y + pt.dist + math.sin((pt.x + wave_offset) * 0.02) * 10
+	end
+
+	if wave_offset < -wave_points[1].x - DIST_BETWEEN_WATER_POINTS then
+		local pt = table.remove(wave_points, 1)
+		local n = table.getn(wave_points)
+		local lastpt = wave_points[n]
+		pt.i = lastpt.i + 1
+		pt.x = (pt.i-1) * DIST_BETWEEN_WATER_POINTS
+		pt.dist = math.sin(pt.x * 0.01) * 100
+		table.insert(wave_points, pt)
 	end
 end
 
 function love.draw()
-	local img = assets.img.bg_storm
-	local sx, sy = love.graphics.getDimensions() / img:getDimensions()
-	love.graphics.setColor(color.val(COL_WHITE))
-	love.graphics.draw(img, 0,0, 0, sx,sy)
+	if not DEBUG_DRAW_NO_BG then
+		local img = assets.img.bg_storm
+		local sx, sy = love.graphics.getDimensions() / img:getDimensions()
+		love.graphics.setColor(color.val(COL_WHITE))
+		love.graphics.draw(img, 0,0, 0, sx,sy)
+	end
 
 	local vertices = {}
 	local n = table.getn(wave_points)
 	local gh = love.graphics.getHeight()
+	local wx = wave_offset
+	local water_mode = "fill"
+	if DEBUG_DRAW_WATER_POLY then water_mode = "line" end
 	love.graphics.setColor(color.val(COL_WATER))
 	for i,pt in ipairs(wave_points) do
 		if i < n then
 			local pt2 = wave_points[i+1]
-			love.graphics.polygon("fill", pt.x,pt.y,pt2.x,pt2.y,pt2.x,gh,pt.x,gh)
+			love.graphics.polygon(water_mode, pt.x+wx,pt.y,pt2.x+wx,pt2.y,pt2.x+wx,gh,pt.x+wx,gh)
 		end
 	end
 end
